758c56a23fe9664e40a5d9e8e89ee10f9501ade7
Kipngetich33 <khalifngeno@gmail.com>
2018-01-22 16:14:08 +0300

    add tests for the tests instance

--- potraitHall/potrait/models.py
+++ potraitHall/potrait/models.py
@@ -1,26 +1,34 @@
 from django.db import models
 
-class Image(models.Model):
-    image = models.ImageField(upload_to = 'articles/',blank =True)
-    image_name = models.CharField(max_length =30)
-    image_description = models.CharField(max_length =3000)
-    location = models.ForeignKey(Location, null = True)
-    category = models.ForeignKey(Category)
-
-    def __str__(self):
-        return self.first_name
-
 class Location(models.Model):
     name = models.CharField(max_length =100)
 
     def __str__(self):
-        return self.first_name
+        return self.name
 
-class Categories(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length =100)
 
     def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'articles/',blank =True)
+    image_name = models.CharField(max_length =30)
+    image_description = models.CharField(max_length =3000)
+    location = models.ForeignKey(Location,null = True)
+    category = models.ForeignKey(Category, null = True) 
+
+    def __str__(self):
         return self.first_name
+
+    def save_images(self):
+        self.save()
+
+    def delete_images(self):
+        self.delete()
+
+
 
 
 
--- potraitHall/potrait/tests.py
+++ potraitHall/potrait/tests.py
@@ -1,6 +1,23 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.test import TestCase
+from .models import Category, Image , Location
 
-# Create your tests here.
+class ImageTestClass(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.image= Image(image = 'imageurl', image_name ='test_image', image_description='image test description')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+
+    def test_save_images(self):
+        self.image.save_images()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
+
+    def test_delete_images(self):
+        self.image.save_images()
+        self.image.delete_images()
+        images = Image.objects.all()
+        self.assertTrue(len(images)==0)
