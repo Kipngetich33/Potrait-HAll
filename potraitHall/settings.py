2dbac5f29af00aa2a3c4a3fd658654e381a67b2c
Kipngetich33 <khalifngeno@gmail.com>
2018-01-29 08:05:42 +0300

    make changes to settings

--- potraitHall/settings.py
+++ potraitHall/settings.py
@@ -129,8 +129,6 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
